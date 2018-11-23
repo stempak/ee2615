f5d1e5608334a0e321abb32f6a37ad247bcad856
stempak <45299687+stempak@users.noreply.github.com>
2018-11-23 22:28:46 +0000

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,9 @@
+//CUSTOMISE OVERLAY
+
+function on1() {
+    document.getElementById("week-1").style.display = "block";
+}
+
+function off1() {
+    document.getElementById("week-1").style.display = "none";
+}
